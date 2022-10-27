d079d02b8f9db72fd3eba1421d7679ca48213da5
NduonyiJackUkitetu <113561767+NduonyiJackUkitetu@users.noreply.github.com>
2022-10-27 16:06:20 -0700

    Add files via upload

---
+++ Client.h
@@ -0,0 +1,17 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class Client{
+public:
+
+	string password_c;
+
+	Client();
+	int for_client();
+};
+#endif
