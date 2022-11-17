8eb092fff979ffea79ebec5d3a3e6cfa0b241d64
Nduonyi Jack Ukitetu <113561767+NduonyiJackUkitetu@users.noreply.github.com>
2022-11-17 10:55:50 -0800

    Update Urepair main.cpp

--- Urepair main.cpp
+++ Urepair main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	char classtype, prompt, retry;
+	string classtype, prompt, retry;
 	cout << setw(70);
 	cout << "TEST RUN BEFORE GUI" << endl;
 	cout << endl;
@@ -19,21 +19,21 @@ int main(int argc, char** argv) {
 		cout << "Enter '0' to start." << endl;
 		cin >> prompt;
 
-		if (prompt == '0') {
-			cout << "why are using this program (e.g Client=1, Talent=2, Admin=3) = ";
+		if (prompt == "0") {
+			cout << "what type of user (e.g Client=1, Talent=2, Admin=3) = ";
 			cin >> classtype;
 
-			if (classtype == '1') {
+			if (classtype == "1") {
 				Client c1;
 				c1.for_client();
 			}
 
-			else if (classtype == '2') {
+			else if (classtype == "2") {
 				Talent t1;
 				t1.for_talent();
 			}
 
-			else  if (classtype == '3') {
+			else  if (classtype == "3") {
 				Admin a1;
 				a1.for_admin();
 			}
@@ -42,7 +42,7 @@ int main(int argc, char** argv) {
 
 			cout << "Enter '1' to quit the app. Enter '0' to continue searching. " << endl;
 			cin >> retry;
-			if (retry == '1') {
+			if (retry == "1") {
 				cout << "Thanks for using the app! Get your equimpment fixed!" << endl;
 				break;
 			}
