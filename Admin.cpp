4c2541a5e11824c9c46190fb08e5a79af5c4f6c8
Nduonyi Jack Ukitetu <113561767+NduonyiJackUkitetu@users.noreply.github.com>
2022-11-29 17:05:24 -0800

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -3,12 +3,13 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <stdio.h>
 using namespace std;
 
 string password_a;
-string a_talent_id, a_pick;
-string a_id, a_first_name, a_last_name, a_state, a_email;
-float a_price, a_rating, a_location;
+string a_talent_id, a_pick, a_textline;
+string a_id, a_first_name, a_last_name, a_state, a_email, a_services;
+float a_price, a_rating;
 
 Admin::Admin() {
 	string password_a = "0";
@@ -18,13 +19,13 @@ void a_readfile()
 {
 	ifstream mydata;
 	mydata.open("talent.dat");
-	while (!mydata.eof())
+	while (mydata >> a_id >> a_first_name >> a_last_name >> a_price >> a_rating >> a_services >> a_state >> a_email)
 	{
-		mydata >> a_id >> a_first_name >> a_last_name >> a_price >> a_rating >> a_location >> a_state >> a_email;
+		
 
 		if (a_id == a_talent_id)
 		{
-			cout << a_id << " " << a_first_name << " " << a_last_name << " $" << a_price << " rating :" << a_rating << " Distance: " << a_location << "km " << "is " << a_state << " Email :" << a_email << endl;
+			cout << a_id << " " << a_first_name << " " << a_last_name << " $" << a_price << " rating :" << a_rating << " Services: " << a_services << " " << "is " << a_state << " Email :" << a_email << endl;
 		}
 
 
@@ -34,28 +35,64 @@ void a_readfile()
 
 void a_chgfile()
 {
-	fstream mydata;
+	ifstream mydata;
 	mydata.open("talent.dat");
-	while (!mydata.eof())
+	ofstream c_data;
+	c_data.open("c_talent.dat");
+	while (mydata >> a_id >> a_first_name >> a_last_name >> a_price >> a_rating >> a_services >> a_state >> a_email)
 	{
-		mydata >> a_id >> a_first_name >> a_last_name >> a_price >> a_rating >> a_location >> a_state >> a_email;
 
-		if (a_id == a_talent_id)
+		if (a_id != a_talent_id)
 		{
-			cout << a_id << " " << a_first_name << " " << a_last_name << " $" << a_price << " rating :" << a_rating << " Distance: " << a_location << "km " << "is " << a_state << " Email :" << a_email << endl;
+			c_data << a_id << " " << a_first_name << " " << a_last_name << " " << a_price << " " << a_rating << " " << a_services << " " << a_state << " " << a_email << endl;
+			
+		}
+		else
+		{
+			c_data.close();
+			c_data.open("c_talent.dat", std::ios::app);
+			cout << a_id << " " << a_first_name << " " << a_last_name << " $" << a_price << " rating :" << a_rating << " Services: " << a_services << " " << "is " << a_state << " Email :" << a_email << endl;
 
 			cout << "New price: ";
 			cin >> a_price;
-			cout << "New location: ";
-			cin >> a_location;
+			cout << "New Services: ";
+			cin >> a_services;
 			cout << "(Available or Booked ): ";
 			cin >> a_state;
 			cout << "New email: ";
 			cin >> a_email;
+			std::ostringstream text_join;
+			text_join << a_id << " " << a_first_name << " " << a_last_name << " " << a_price << " " << a_rating << " " << a_services << " " << a_state << " " << a_email << endl;
+			std::string a_textline = text_join.str();
+			c_data << a_textline;
 		}
 
 	}
+
 		mydata.close();
+		c_data.close();
+
+		//std::rename("c_talent.dat", "talent.dat");
+
+		ifstream in("c_talent.dat");
+		ofstream out1("talent.dat");
+
+		while (in >> a_id >> a_first_name >> a_last_name >> a_price >> a_rating >> a_services >> a_state >> a_email)
+		{
+			// string to extract line from 
+			// file.txt
+			string text;
+
+			// Writing the extracted line in 
+			// file2.txt
+			out1 << a_id << " " << a_first_name << " " << a_last_name << " " << a_price << " " << a_rating << " " << a_services << " " << a_state << " " << a_email << endl;
+		}
+		in.close();
+		out1.close();
+
+		std::ofstream ofs;
+		ofs.open("c_talent.dat", std::ofstream::out | std::ofstream::trunc);
+		ofs.close();
 }
 
 int Admin::for_admin()
